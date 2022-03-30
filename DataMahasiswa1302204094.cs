6b48ad1f56fd134334b704a77b1890677755474f
Howler Nofelle <hilmy@wahidin.net>
2022-03-30 21:11:46 +0700

    menambahkan class DataMahasiswa1302204094

---
+++ DataMahasiswa1302204094.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul_kelompok7
+{
+    internal class DataMahasiswa1302204094
+    {
+        public void ReadJSON()
+        {
+         Console.WriteLine("Nama Naufel Hilmy dengan nim 1302204094 dari fakultas informatika");
+        }
+    }
+}
